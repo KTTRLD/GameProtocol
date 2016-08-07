817703b056098091bf2a60963f39668b36961c73
KTTRLD <dokcarlos@icloud.com>
2016-08-07 20:08:33 +0200

    Time Calculation

---
+++ Copy2 of GameProtocolProjekt/src/com/gameprojekt/gottfried/model/ElapsedTimeBetweenDates.java
@@ -0,0 +1,37 @@
+package com.gameprojekt.gottfried.model;
+
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+public class ElapsedTimeBetweenDates {
+
+    public static void main(String []args) {
+        Date startDateTime = new Date(System.currentTimeMillis() - 123456789);
+        Date endDateTime = new Date();
+
+        Map<TimeUnit,Long> result = computeDiff(startDateTime, endDateTime);
+        System.out.println(result);
+
+        System.out.println("Days: " + result.get(TimeUnit.DAYS));
+        System.out.println("Hours: " + result.get(TimeUnit.HOURS));
+        System.out.println("Minutes: " + result.get(TimeUnit.MINUTES));
+        System.out.println("Seconds: " + result.get(TimeUnit.SECONDS));
+        System.out.println("MilliSeconds: " + result.get(TimeUnit.MILLISECONDS));
+    }
+
+    public static Map<TimeUnit,Long> computeDiff(Date date1, Date date2) {
+        long diffInMilliSeconds = date2.getTime() - date1.getTime();
+        List<TimeUnit> units = new ArrayList<TimeUnit>(EnumSet.allOf(TimeUnit.class));
+        Collections.reverse(units);
+        Map<TimeUnit,Long> result = new LinkedHashMap<TimeUnit,Long>();
+        long milliSecondsRest = diffInMilliSeconds;
+        for (TimeUnit unit : units) {
+            long diff = unit.convert(milliSecondsRest,TimeUnit.MILLISECONDS);
+            long diffInMilliSecondsForUnit = unit.toMillis(diff);
+            milliSecondsRest = milliSecondsRest - diffInMilliSecondsForUnit;
+            result.put(unit,diff);
+        }
+        return result;
+    }
+
+}
